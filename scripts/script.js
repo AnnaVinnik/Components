33c6831648c9b5dd969589addfa044df0a84e70e
Anna <vinnik.anna@rambler.ru>
2022-08-15 12:49:59 +0700

    Добавлена ф-ция создания слайдов

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,21 @@ const slider = {
     images: ["img/1.jpg", "img/2.jpg"],
     linkOnPage: document.querySelector(".slider"),
 
-    paint() {
+    create() {
         this.images.forEach( (elem) => {
+            createSlide.call(this, elem);
+        });
 
+        function createSlide(elem) {
             let slide = document.createElement('img');
 
             slide.src = elem;
             slide.classList.add("slide");
 
             this.linkOnPage.append(slide);
-        })
+        }
+
     },
 };
 
-slider.paint();
+slider.create();
